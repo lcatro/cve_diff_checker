565cd4dbcd60a486ac388942e97b02b473e82120
FrostNova2021 <lcatro@sina.cn>
2022-01-20 11:40:15 +0800

    refactor code for workflow

--- cve_resolver.py
+++ cve_resolver.py
@@ -33,13 +33,7 @@ def load_diff_from_cve_file(cve_file_path):
                 url_list.append(url.text)
 
         if url_list:
-            patch_url_list[title] = url_list # (description,url_list)
-
-        #print(title)
-        #print(url_list)
-        #exit()
-
-    #print(json.dumps(patch_url_list,indent=2))
+            patch_url_list[title] = url_list
 
     return patch_url_list
 
@@ -68,7 +62,6 @@ def make_esixt_cve_diff_record(all_cve_list):
         
         for cve,url_list in patch_url_list.items():
             for url in url_list:
-                print(cve,url)
                 current_dir = os.path.dirname(os.path.abspath(__file__))
                 diff_dir = os.path.join(current_dir,'diff')
 
@@ -80,28 +73,31 @@ def make_esixt_cve_diff_record(all_cve_list):
                 if not os.path.exists(file_path):
                     continue
 
+                print('Search New CVE Patch Record ==>>',cve,url)
+
                 if not result.get(cve):
                     result[cve] = [file_path]
                 else:
                     result[cve].append(file_path)
 
-    file = open('.\\diff\\cve_diff_map.txt','w')
+    cve_diff_map = os.path.join('diff','cve_diff_map')
+
+    file = open(cve_diff_map,'w')
     file.write(json.dumps(result,indent=2))
     file.close()
 
     return result
 
-all_cve_list = [
-    '.\\cve_list\\allitems-cvrf-year-2015.xml' ,
-    '.\\cve_list\\allitems-cvrf-year-2016.xml' ,
-    '.\\cve_list\\allitems-cvrf-year-2018.xml' ,
-    '.\\cve_list\\allitems-cvrf-year-2019.xml' ,
-    '.\\cve_list\\allitems-cvrf-year-2020.xml' ,
-    '.\\cve_list\\allitems-cvrf-year-2021.xml' 
-]
 
-#for cve_index in all_cve_list:
-#    try_download(cve_index)
+if __name__ == '__main__':
+    cve_list_file = os.listdir('./cve_list')
+    all_cve_list = []
 
-make_esixt_cve_diff_record(all_cve_list)
+    for cve_file in cve_list_file:
+        if not cve_file.startswith('allitem'):
+            continue
+
+        all_cve_list.append(os.path.join('cve_list',cve_file))
+
+    make_esixt_cve_diff_record(all_cve_list)
 
